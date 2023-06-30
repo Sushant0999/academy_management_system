b4dc1281bc5172f686a7ad84dfae16dbb57e6c60
Sushant0999 <sushantraj215@gmail.com>
2023-06-30 09:40:10 +0530

    FIRST

---
+++ src/main/java/managment/netsmartz/NetsmartzApplication.java
@@ -0,0 +1,13 @@
+package managment.netsmartz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NetsmartzApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NetsmartzApplication.class, args);
+	}
+
+}
---
+++ src/test/java/managment/netsmartz/NetsmartzApplicationTests.java
@@ -0,0 +1,13 @@
+package managment.netsmartz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NetsmartzApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
