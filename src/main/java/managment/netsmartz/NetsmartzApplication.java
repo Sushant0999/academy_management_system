56d52012566b2c421f284b1b8576c72f66c10341
vanshika734 <vanshikamisra73@gmail.com>
2023-07-04 11:52:51 +0530

    yoyo

--- src/main/java/managment/netsmartz/NetsmartzApplication.java
+++ src/main/java/managment/netsmartz/NetsmartzApplication.java
@@ -6,6 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class NetsmartzApplication {
 
+	/*vaaaanshika*/
+
 	public static void main(String[] args) {
 		SpringApplication.run(NetsmartzApplication.class, args);
 	}
